2233890506c270601a5e199a66767aca3a510622
T-Kepley-McGuire <kepley.mcguire.mail@gmail.com>
2023-08-26 10:46:17 -0700

    Added shell controls, updated UI. No functionality change yet

--- renderer.js
+++ renderer.js
@@ -1,8 +1,9 @@
-export async function render({pixelWidth, pixelHeight}, pixelArray) {
+export async function render({ pixelWidth, pixelHeight }, pixelArray) {
   if (!(pixelArray instanceof Uint8ClampedArray)) {
     throw Error("pixel array must be of type Uint8ClampedArray");
   }
-  const canvas = document.querySelector("#gpuCanvas");
+  const canvas = document.querySelector("#display");
+  const container = document.querySelector("#display-container");
   const context = canvas.getContext("2d");
 
   // Initialize a new ImageData object
@@ -11,7 +12,15 @@ export async function render({pixelWidth, pixelHeight}, pixelArray) {
   const imageBitmap = await createImageBitmap(imageData);
 
   // Draw image data to the canvas
-  const width = 800;
-  context.drawImage(imageBitmap, 0, 0, width, width * pixelHeight / pixelWidth);
-  //context.putImageData(imageData, 20, 20);
+  const width = container.clientWidth;
+  canvas.width = width;
+  canvas.height = (width * pixelHeight) / pixelWidth;
+
+  context.drawImage(
+    imageBitmap,
+    0,
+    0,
+    width,
+    (width * pixelHeight) / pixelWidth
+  );
 }
